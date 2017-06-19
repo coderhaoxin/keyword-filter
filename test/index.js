369efd929728c9395495950741c1c2df48b30f9c
haoxin <haoxins@outlook.com>
2017-06-19 13:09:34 +0800

    lint

--- test/index.js
+++ test/index.js
@@ -1,54 +1,55 @@
-'use strict';
+'use strict'
 
-var equal = require('assert').strictEqual ,
-  KeywordFilter = require('..');
+const equal = require('assert').strictEqual
+const KeywordFilter = require('..')
 
-var content1 = 'what is the best lang, go or js?你呀个妹，咿呀我哈噶',
-  content2 = '快乐的一天, hello my name is haoxin',
-  content3 = 'what is the best lang, go or js?\n' + 
-					'你呀个妹，咿呀我哈噶',
-  keyArrays = ['go', 'js', 'lang', '我哈', '你呀'];
+let content1 = 'what is the best lang, go or js?你呀个妹，咿呀我哈噶'
+let content2 = '快乐的一天, hello my name is haoxin'
+let content3 = 'what is the best lang, go or js?\n' +
+  '你呀个妹，咿呀我哈噶'
 
-var filter = new KeywordFilter();
+let keyArrays = ['go', 'js', 'lang', '我哈', '你呀']
 
-filter.init(keyArrays);
+const filter = new KeywordFilter()
+
+filter.init(keyArrays)
 
 describe('## keyword filter', function() {
   describe('# getOccurances', function() {
-	 it('should map line numers and discovered values', function() {
-		 var results = filter.getOccurances(content3);
-		 equal(results[0].line, 1);
-		 equal(results[0].value, 'lang');
-		 equal(results[3].line, 2);
-		 equal(results[3].value, '你呀');
-		 
-	 })
-  });
+    it('should map line numers and discovered values', function() {
+      const results = filter.getOccurances(content3)
+      equal(results[0].line, 1)
+      equal(results[0].value, 'lang')
+      equal(results[3].line, 2)
+      equal(results[3].value, '你呀')
+    })
+  })
+
   describe('# hasKeyword', function() {
     it('should be true', function() {
-      equal(filter.hasKeyword(content1), true);
-    });
+      equal(filter.hasKeyword(content1), true)
+    })
 
     it('should be false', function() {
-      equal(filter.hasKeyword(content2), false);
-    });
-  });
+      equal(filter.hasKeyword(content2), false)
+    })
+  })
 
   describe('# replaceKeyords', function() {
     it('should replace keywords with char', function() {
-      equal(filter.replaceKeywords(content1, '*'), 'what is the best ****, ** or **?**个妹,咿呀**噶');
-    });
+      equal(filter.replaceKeywords(content1, '*'), 'what is the best ****, ** or **?**个妹,咿呀**噶')
+    })
 
     it('should not replace keywords with char', function() {
-      equal(filter.replaceKeywords(content2, '*'), content2);
-    });
+      equal(filter.replaceKeywords(content2, '*'), content2)
+    })
 
     it('should replace keywords with string', function() {
-      equal(filter.replaceKeywords(content1, 'happy'), 'what is the best happy, happy or happy?happy个妹,咿呀happy噶');
-    });
+      equal(filter.replaceKeywords(content1, 'happy'), 'what is the best happy, happy or happy?happy个妹,咿呀happy噶')
+    })
 
     it('should not replace keywords with string', function() {
-      equal(filter.replaceKeywords(content2, 'happy'), content2);
-    });
-  });
-});
+      equal(filter.replaceKeywords(content2, 'happy'), content2)
+    })
+  })
+})
